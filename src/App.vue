3636cd58200fc07f3e79578d991d570217776b07
erick_braga <@Erickmatheus07>
2022-04-09 11:59:40 -0300

    aula finaliada

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,52 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+  <div class="corpo">
+    <h1 class="centralizado">{{ titulo }}</h1>
+
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="(foto, index) of fotos" :key="index">
+        <img :src="foto.url" :alt="foto.tutilo" />
+      </li>
     </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+
   </div>
 </template>
 
+
 <script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Alaaaa, o QA causando!'
+  export default {
+
+    data() {
+      return {
+        titulo: "Alurapic",
+        fotos: [],
+      };
+    },
+
+    created() {
+      this.$http.get('http://localhost:3000/v1/fotos')
+        .then(res => res.json())
+        .then(fotos => this.fotos = fotos, err => console.log(err));
     }
-  }
-}
+  };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    width: 96%;
+    margin: 0 auto;
+  }
 
-h1, h2 {
-  font-weight: normal;
-}
+  .centralizado {
+    text-align: center;
+  }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+  .lista-fotos {
+    list-style: none;
+  }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+  .lista-fotos .lista-fotos-item {
+    display: inline-block;
+  }
 
-a {
-  color: #42b983;
-}
 </style>
